97cfc31bc567169948dd58542a1d781bd345a0bb
leevermy <risnotr@gmail.com>
2024-05-24 17:21:38 +0300

    add  task

--- task04.cpp
+++ task04.cpp
@@ -1,14 +1,69 @@
 #include <iostream>
+#include <string>
+#include <ctime>
 
-struct character {
- std::string name;
- int armor = 0;
- int health = 100;   
+
+struct Character {
+    std::string name;
+    int health_point;
+    int armor_point;
+    int damage_point;
 };
 
-int main() {
-    character person = {"john", person.health=10, person.armor = 10};
+char* MakeField();
 
-    std::cout << person.name << " " << person.armor << " " << person.health; 
-    return 0;
+int main() {
+    const int field_size = 20;
+    char field[field_size][field_size];
+
+    MakeField();
+
+    char player = 'P';
+    char enemy = 'E';
+    char empty_place = '.';
+
+    //действия игрока
+    PrintField(field, field_size);
+}
+
+MakeField() {
+    srand(time(nullptr));
+    Character enemies[5];
+    for (int i = 0; i < 5; ++i) {
+        enemies[i].name = "Enemy #" + std::to_string(i);
+        enemies[i].health_point = rand() % 101 + 50;
+        enemies[i].armor_point = rand() % 51;
+        enemies[i].damage_point = rand() % 16 + 15;
+    }
+
+    Character player;
+    std::cin >> player.name;
+    std::cin >> player.health_point;
+    std::cin >> player.armor_point;
+    std::cin >> player.damage_point;
+
+    // разместить на поле
+}
+
+bool PlayerMove(int x, int y) {
+    char direction;
+    std::cin >> direction;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    direction = toupper(direction);
+
+    //L, R, U, D
+    if (direction == 'L') --x;
+    else if (direction == 'R') ++x;
+    else if (direction == 'U') --y;
+    else if (direction == 'D') ++y;
+    else {
+        std::cout << "Unknown command" << std::endl;
+        return false;
+    }
+    return true;
+}
+
+void EnemiesMove(Character enemies[], int size) {
+
 }
