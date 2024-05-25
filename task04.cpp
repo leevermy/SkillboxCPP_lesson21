dc11bfdcdcdbc1b8a34d9fbfc9cb2848dd7b01ce
Aleksey Goryanin <risnotr@gmail.com>
2024-05-25 13:05:16 +0300

    shall redone

--- task04.cpp
+++ task04.cpp
@@ -1,8 +1,10 @@
-#include <ctime>
+#include <algorithm>
 #include <cstring>
+#include <ctime>
+#include <filesystem>
+#include <fstream>
 #include <iostream>
 #include <string>
-#include <algorithm>
 #include <vector>
 
 using namespace std;
@@ -14,13 +16,15 @@ struct Character {
   int damage_point;
   int pos_x;
   int pos_y;
+  bool is_hero = 0;
 };
 
 void InitializeMap(vector<vector<char>>& map, const int& size) {
-    map.resize(size, vector<char>(size, '.'));
+  map.resize(size, vector<char>(size, '.'));
 }
 
-void CreateEnemy(vector<vector<char>>& map, Character& enemy, int& enemy_number) {
+void CreateEnemy(vector<vector<char>>& map, Character& enemy,
+                 int& enemy_number) {
   std::string name = "Enemy #" + std::to_string(enemy_number + 1);
   if (name.length() >= sizeof(enemy.name)) {
     std::cerr << "Name is too long!" << std::endl;
@@ -54,163 +58,213 @@ void CreatePlayer(vector<vector<char>>& map, Character& player) {
     player.pos_y = rand() % map.size();
     if (map[player.pos_y][player.pos_x] == '.') break;
   }
+
+  player.is_hero = true;
 }
 
 void PrintMap(const vector<vector<char>>& map, const int& size) {
-    cout << endl;
-    for (int i = 0; i < size + 1; ++i) {
-        cout << "# ";
-    }
-    cout << "#" << endl;
+  cout << endl;
+  for (int i = 0; i < size + 1; ++i) {
+    cout << "# ";
+  }
+  cout << "#" << endl;
 
-    for (const auto & row : map) {
-        cout << "#";
-        for (const auto& col : row) {
-            cout << ' ' << col;
+  for (const auto& row : map) {
+    cout << "#";
+    for (const auto& col : row) {
+      cout << ' ' << col;
+    }
+    cout << " #" << endl;
+  }
+
+  for (int i = 0; i < size + 1; ++i) {
+    cout << "# ";
+  }
+  cout << "#\n" << endl;
+}
+
+void PlaceCharacter(vector<vector<char>>& map, const Character& character,
+                    const char symbol) {
+  map[character.pos_y][character.pos_x] = symbol;
+}
+
+void InputFromUser(string& command) {
+  do {
+    cout << "Enter 'A', 'D', 'W', 'S' to move or 'SAVE', 'LOAD' to save or "
+            "load: ";
+    cin >> command;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    transform(command.begin(), command.end(), command.begin(),
+              [](unsigned char ch) { return toupper(ch); });
+  } while (command != "A" && command != "D" && command != "W" &&
+           command != "S" && command != "SAVE" && command != "LOAD");
+}
+
+bool IsMovingCommand(const string& command) {
+  return command == "A" || command == "D" || command == "W" || command == "S";
+}
+
+void Load(Character& player, vector<Character>& enemies) {
+  filesystem::path file_path = "./save.bin";
+  ifstream in_file(file_path, ios::binary);
+
+  if (!in_file.is_open()) {
+    cerr << "Failed to open file. Probably, file doesn't exist" << endl;
+    return;
+  }
+
+  in_file.read(reinterpret_cast<char*>(&player), sizeof(Character));
+
+  size_t enemies_count;
+  in_file.read(reinterpret_cast<char*>(&enemies_count), sizeof(size_t));
+
+  enemies.clear();
+  enemies.resize(enemies_count);
+  for (size_t i = 0; i < enemies_count; ++i) {
+    in_file.read(reinterpret_cast<char*>(&enemies[i]), sizeof(Character));
+  }
+
+  in_file.close();
+  cout << "The game is loaded successfully" << endl;
+}
+
+void Save(const Character& player, const vector<Character>& enemies) {
+  filesystem::path file_path = "./save.bin";
+  ofstream out_file(file_path, ios::binary);
+
+  if (!out_file.is_open()) {
+    cerr << "Failed to open file." << endl;
+    return;
+  }
+
+  out_file.write(reinterpret_cast<const char*>(&player), sizeof(Character));
+
+  size_t enemy_count = enemies.size();
+  out_file.write(reinterpret_cast<const char*>(&enemy_count),
+                 sizeof(enemy_count));
+
+  for (const auto& enemy : enemies) {
+    out_file.write(reinterpret_cast<const char*>(&enemy), sizeof(Character));
+  }
+
+  out_file.close();
+  cout << "The game saved successfully" << endl;
+}
+
+void Attack(Character& attacker, Character& defender) {
+  defender.armor_point -= attacker.damage_point;
+  if (defender.armor_point < 0) {
+    defender.health_point += defender.armor_point;
+    defender.armor_point = 0;
+  }
+
+  cout << attacker.name << " attacks " << defender.name << " for "
+       << attacker.damage_point << " damage!" << endl;
+}
+
+void CharacterAction(vector<vector<char>>& map, Character& character,
+                     Character& player, vector<Character>& enemies,
+                     const string& command) {
+  int map_size = map.size();
+  int new_x = character.pos_x;
+  int new_y = character.pos_y;
+
+  if (command == "W")
+    --new_y;
+  else if (command == "S")
+    ++new_y;
+  else if (command == "A")
+    --new_x;
+  else if (command == "D")
+    ++new_x;
+
+  if (new_x >= 0 && new_x < map_size && new_y >= 0 && new_y < map_size) {
+    if (map[new_y][new_x] == '.') {
+      map[character.pos_y][character.pos_x] = '.';
+      character.pos_x = new_x;
+      character.pos_y = new_y;
+    } else if (character.is_hero && map[new_y][new_x] == 'E') {
+      for (auto& enemy : enemies) {
+        if (enemy.pos_x == new_x && enemy.pos_y == new_y) {
+          Attack(character, enemy);
+          break;
         }
-        cout << " #" << endl;
+      }
+    } else if (!character.is_hero && map[new_y][new_x] == 'P') {
+      Attack(character, player);
     }
-
-    for (int i = 0; i < size + 1; ++i) {
-        cout << "# ";
-    }
-    cout << "#\n" << endl;
+  }
 }
 
-void PlaceCharacter(vector<vector<char>>& map, const Character& character, const char symbol) {
-    map[character.pos_y][character.pos_x] = symbol;
-}
-
-void InputFromUser(string& symbol) {
-    do {
-        cout << "Enter \'A\', \'D\', \'W\', \'S\' to move ";
-        cout << "or \'SAVE\', \'LOAD\' to save or download: ";
-        cin >> symbol;
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
-        transform(symbol.begin(), symbol.end(), symbol.begin(), [](unsigned char ch) {
-            return toupper(ch);
-        });
-
-    } while (symbol != "A" && symbol != "D" && symbol != "W" && symbol != "S" && symbol != "SAVE" && symbol != "LOAD");
-    
-
-
-}
-
-bool IsMovingSymbol(const string& symbol) {
-    return symbol == "A" || symbol == "D" || symbol == "W" || symbol == "S";
-}
-
-void Load();
-
-void Save();
-
-void MoveCharacter(vector<vector<char>>& map, Character& character, const string& symbol) {
-    int map_size = map.size();
-    int new_x = character.pos_x;
-    int new_y = character.pos_y;
-
-    if (symbol == "W") --new_y;
-    else if (symbol == "S") ++new_y;
-    else if (symbol == "A") --new_x;
-    else if (symbol == "D") ++new_x;
-
-    if (new_x >= 0 && new_x < map_size && new_y >= 0 && new_y < map_size) {
-        if (map[new_y][new_x] == '.') {
-            map[character.pos_y][character.pos_x] = '.';
-            character.pos_x = new_x;
-            character.pos_y = new_y;
-        } else if (map[new_y][new_x] == 'P' || map[new_y][new_x] == 'E') {
-            
-        }
-    }
-
-}
-
-void Attack() {
-    player.armor_point -= enemy.damage_point;
-            if (player.armor_point < 0) {
-                player.health_point += player.armor_point;
-                player.armor_point = 0;
-            }
-}
-
-
-bool IsDead(const Character& character) {
-    return character.health_point <= 0;
-}
-
-
+bool IsDead(const Character& character) { return character.health_point < 1; }
 
 bool GameOver(const Character& player, const vector<Character>& enemies) {
-    if (player.health_point <= 0) {
-        cout << "You are defeted!" << endl;
-        return true;
-    }
-    
-    for (const auto& enemy : enemies) {
-        if (enemy.health_point > 0) {
-            return false;
-        }
-    }
-    cout << "All enemies are dead! You win!" << endl;
+  if (player.health_point <= 0) {
+    cout << "You are defeated!" << endl;
     return true;
+  }
+
+  for (const auto& enemy : enemies) {
+    if (enemy.health_point > 0) {
+      return false;
+    }
+  }
+  cout << "All enemies are dead! You win!" << endl;
+  return true;
 }
 
 int main() {
-    const int map_size = 20;
-    vector<vector<char>> map;
+  srand(static_cast<unsigned int>(time(0)));
+
+  const int map_size = 20;
+  vector<vector<char>> map;
+  InitializeMap(map, map_size);
+
+  Character player;
+  CreatePlayer(map, player);
+
+  int enemies_amount = 5;
+  vector<Character> enemies(enemies_amount);
+  for (int i = 0; i < enemies_amount; ++i) {
+    CreateEnemy(map, enemies[i], i);
+  }
+
+  while (true) {
     InitializeMap(map, map_size);
-
-    Character player;
-    CreatePlayer(map, player);
-
-    int enemies_amount = 5;
-    vector<Character> enemies(enemies_amount);
-    for (int i = 0; i < enemies_amount; ++i) {
-        CreateEnemy(map, enemies[i], i);
+    PlaceCharacter(map, player, 'P');
+    for (const auto& enemy : enemies) {
+      if (!IsDead(enemy)) {
+        PlaceCharacter(map, enemy, 'E');
+      }
     }
 
+    PrintMap(map, map_size);
 
-    while (true) {
-        PlaceCharacter(map, player, 'P');
-        for (const auto& enemy : enemies) {
-            if (IsDead(enemy)) {
-                PlaceCharacter(map, enemy, '.');
-            } else {
-                PlaceCharacter(map, enemy, 'E');
-            }
-        }
+    string command;
+    InputFromUser(command);
 
-        PrintMap(map, map_size);
-
-        string symbol;
-        InputFromUser(symbol); 
-
-        if (IsMovingSymbol(symbol)) {
-            MoveCharacter(map, player, symbol);
-        } else {
-            if (symbol == "SAVE") {
-                Save();
-            } else {
-                Load();
-                continue;
-            }
-        }
-        
-
-        for (auto& enemy : enemies) {
-            string symbol = string(1, "L,R,D,U"[rand() % 4]);
-            MoveCharacter(map, enemy, symbol);
-        }
-        
-
-
-
-        if (GameOver(player, enemies)) {
-            break;
-        }
+    if (IsMovingCommand(command)) {
+      CharacterAction(map, player, player, enemies, command);
+    } else {
+      if (command == "SAVE") {
+        Save(player, enemies);
+      } else if (command == "LOAD") {
+        Load(player, enemies);
+        continue;
+      }
     }
-    return 0;
+
+    for (auto& enemy : enemies) {
+      if (!IsDead(enemy)) {
+        string command(1, "WSAD"[rand() % 4]);
+        CharacterAction(map, enemy, player, enemies, command);
+      }
+    }
+
+    if (GameOver(player, enemies)) {
+      break;
+    }
+    map.clear();
+  }
+  return 0;
 }
